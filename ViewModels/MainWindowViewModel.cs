c66aee3e3e38e25b500604cd9c6bd87be6645d20
semenov ramazan <rombik.s@inbox.ru>
2022-03-09 12:22:37 +0300

    Добавьте файлы проекта.

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+using WpfApplicationHost.Models;
+using WpfApplicationHost.Views;
+
+namespace WpfApplicationHost.ViewModels
+{
+    /// <summary>
+    /// <see cref="MainWindow"/> view model.
+    /// </summary>
+    public class MainWindowViewModel
+    {
+        /// <summary>
+        /// Example application tabs.
+        /// </summary>
+        public IEnumerable<TabModel> TabModels { get; private set; } = new[]
+        {
+            new TabModel("1",@"C:\Users\lenovo\source\repos\TaskDataGrid\TaskDataGrid\bin\Debug\TaskDataGrid.exe"),
+            new TabModel("2",@"C:\Users\lenovo\source\repos\Modern_Design\Modern_Design\bin\Debug\Modern_Design.exe")
+            //new TabModel("Write", $@"{Environment.GetFolderPath(Environment.SpecialFolder.Windows)}\write.exe"),
+            //new TabModel("Regedit", $@"{Environment.GetFolderPath(Environment.SpecialFolder.Windows)}\regedit.exe"),
+        };
+    }
+}
